b82d8ea6b62b2f116c72d8d5826fb7cd9c13f786
冯迪 <oabern@163.com>
2026-05-20 11:13:50 +0800

    refactor(example code): remove example code

--- src/main/java/com/secretlab/exercise/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.secretlab.exercise.repository;
-
-import com.secretlab.exercise.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
