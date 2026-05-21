633dad0162b91058626d1eab7d75a2b9986791d0
冯迪 <oabern@163.com>
2026-05-21 16:38:00 +0800

    feat(homecontroller):
    update push time

--- src/main/java/com/secretlab/exercise/controller/HomeController.java
+++ src/main/java/com/secretlab/exercise/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-  private static final String LATEST_MODIFIED_TIME = "2026-05-21 15:40:00";
+  private static final String LATEST_MODIFIED_TIME = "2026-05-21 16:40:00";
 
   @GetMapping("/")
   public String home() {
